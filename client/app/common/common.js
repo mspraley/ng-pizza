72f3353a3c50c8754ce1338a223c6e1c116476b9
Matt Spraley <Matt@MATTHEWs-MacBook-Pro.local>
2016-09-01 14:44:54 -0400

    remove unused component

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,16 +1,16 @@
 import angular from 'angular';
-import Sidebar from './sidebar/sidebar';
 import Pizzabox from './pizzabox/pizzabox';
 import Pizza from './pizza/pizza';
 import SizeSelector from './sizeselector/sizeselector';
 import Toppings from './toppings/toppings';
+import Extras from './extras/extras';
 
 let commonModule = angular.module('app.common', [
-  Sidebar,
   Pizzabox,
   Pizza,
   SizeSelector,
-  Toppings
+  Toppings,
+  Extras
 ])
   
 .name;
